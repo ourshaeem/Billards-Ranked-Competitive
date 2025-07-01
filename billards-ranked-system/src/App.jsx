d8db0ef64de3bda721d4b1f64b6271e65197d9b7
Shaeem <obeyyoungcollege@gmail.com>
2025-07-01 11:04:59 -0400

    jhon jay commit

--- billards-ranked-system/src/App.jsx
+++ billards-ranked-system/src/App.jsx
@@ -2,13 +2,17 @@
 import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
 import Card from "./Card.jsx"
+import Button1 from "./button1.jsx"
 function App() {
   return(
     <>
+    
     <Card></Card>
     <Header></Header>
     <Footer></Footer>
-    
+    <Button1></Button1>
+
+
     </>
     
   )
---
+++ billards-ranked-system/src/Button1.jsx
@@ -0,0 +1,6 @@
+function Button1 (){
+
+    return(<button className = "button"> Start </button>);
+}
+
+export default Button1
